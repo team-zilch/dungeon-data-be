99830ec8038e1028126541a4be95624f111dc849
Will Fritts <waf515@gmail.com>
2019-05-13 14:19:16 -0700

    monster route works, api providing data

--- server.js
+++ server.js
@@ -11,14 +11,52 @@ const app = express();
 //----Update with heroku deployment and react page-------------------->
 const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.static('./public'))
+app.use(express.static('./public'));
 
 //-------Database Setup------------------------------------------------>
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
+//----------Global Variables------------------------------------------>
+const baseUrl = 'http://dnd5eapi.co/api/monsters';
+
 //--------Routes------------------------------------------------------->
-app.get('/',(request,response) => response.send('Server Works'));
+app.get('/', homePage);
+app.get('/monster', monsterData);
+
+
+//-------Route Functions---------------------------------------------->
+function homePage(request, response){
+  response.send('home page route works');
+}
+
+function monsterData(request, response){
+  return superagent.get('http://dnd5eapi.co/api/monsters')
+    .then(result => {
+      const monsterData = result.body;
+      console.log(monsterData);
+      response.send(monsterData);
+    });
+  
+  // response.send('monster route works');
+}
+
+//---------Constructor functions--------------------------------------->
+
+// function monster(){
+  
+// }
+
+//--------Helper functions------------------------------------------>
+
+// function queryDB(queryData, response){
+//   let sqlStatement = ;
+//   let value = ;
+// }
+
+// function insertIntoDB(queryData, response){
+
+// }
 
 
 app.use('*', (request,response) => response.send('Sorry , That route dosent exit. '));
