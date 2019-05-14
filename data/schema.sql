af4d90c246835e6528d536fb5f2c6525b5dfdb7d
Will Fritts <waf515@gmail.com>
2019-05-14 13:59:09 -0700

    updated schema to reflect new constructor function

--- data/schema.sql
+++ data/schema.sql
@@ -1,7 +1,12 @@
-DROP TABLE IF EXISTS monsters
+DROP TABLE IF EXISTS monsters;
 
 CREATE TABLE IF NOT EXISTS monsters (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  url VARCHAR(255)
+  size VARCHAR(255),
+  type VARCHAR(255),
+  armor_class VARCHAR(255),
+  hit_points VARCHAR(255),
+  hit_dice VARCHAR(255),
+  challenge_rating VARCHAR(255)
 );
